7be60d16f471b866106cab3d23ff5864479c0883
leezhongjun <80515759+leezhongjun@users.noreply.github.com>
2021-03-13 09:24:58 +0800

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -3,9 +3,11 @@
 <head>
   <meta charset="UTF-8">
   <link rel="stylesheet" href="styles.css">
+  <link rel="icon" type="image/png" href="favpng_angle-pattern.png">
 <title>Action Page</title>
 </head>
 <body>
-  
+  Welcome <?php echo $_POST["name"]; ?><br>
+  Your email address is: <?php echo $_POST["email"]; ?>
 </body>
 </html>  
